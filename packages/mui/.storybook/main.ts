1669473275ef4ffc67bc0653365ac55a4f8aa605
nicolasjott <nicobox2002@gmail.com>
2024-04-18 11:06:10 -0400

    Update Storybook configuration in mui package

--- packages/mui/.storybook/main.ts
+++ packages/mui/.storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from "@storybook/react-vite";
+import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
@@ -15,6 +17,16 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: "tag",
+  },
+  viteFinal: async (config) => {
+    config.plugins?.push(
+      /** @see https://github.com/aleclarson/vite-tsconfig-paths */
+      tsconfigPaths({
+        projects: [path.resolve(path.dirname(__dirname), "tsconfig.json")],
+      })
+    );
+
+    return config;
   },
 };
 export default config;
