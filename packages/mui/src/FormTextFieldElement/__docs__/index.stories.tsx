3df54c4be555206af86aec34cf9f5961b7f33ba8
nicolasjott <nicobox2002@gmail.com>
2024-04-18 11:06:39 -0400

    Add FormTextFieldElement component to mui package

---
+++ packages/mui/src/FormTextFieldElement/__docs__/index.stories.tsx
@@ -0,0 +1,116 @@
+import FormContainer from "@/FormContainer";
+import FormPasswordElement from "@/FormPasswordElement";
+import { SubmitButton } from "@/StoryBook/SubmitButton";
+import { Box } from "@mui/material";
+import { action } from "@storybook/addon-actions";
+import type { Meta, StoryObj } from "@storybook/react";
+import { default as FormTextFieldElement } from "..";
+
+const meta = {
+  title: "FormTextFieldElement",
+  component: FormTextFieldElement,
+  decorators: [FormWrapper],
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof FormTextFieldElement>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+function FormWrapper(Story) {
+  return (
+    <FormContainer
+      defaultValues={{
+        "default-text-field": "Test Data",
+        "default-email-field": "john@example.com",
+        "number-text-field": 6,
+        "default-first-name-field": "John",
+        "default-last-name-field": "Doe",
+      }}
+      onSuccess={action("submit")}
+    >
+      <Box width={"300px"}>
+        <Story />
+        <br />
+        <SubmitButton />
+      </Box>
+    </FormContainer>
+  );
+}
+
+export const Default: Story = {
+  args: {
+    label: "Label",
+    name: "core",
+    fullWidth: true,
+  },
+};
+
+export const Basic = () => (
+  <>
+    <FormTextFieldElement
+      required
+      fullWidth
+      margin={"normal"}
+      label={"First Name"}
+      name={"firstName"}
+    />
+    <br />
+    <FormTextFieldElement
+      required
+      fullWidth
+      margin={"normal"}
+      label={"Last Name"}
+      name={"lastName"}
+    />
+    <br />
+    <FormTextFieldElement
+      required
+      fullWidth
+      type={"email"}
+      margin={"normal"}
+      label={"Email"}
+      name={"email"}
+    />
+    <br />
+    <FormPasswordElement
+      required
+      fullWidth
+      name="password"
+      label="Password"
+      margin="normal"
+    />
+  </>
+);
+
+export const PreDefined = () => (
+  <>
+    <FormTextFieldElement
+      required
+      fullWidth
+      margin={"dense"}
+      label={"First Name"}
+      name={"default-first-name-field"}
+    />
+    <br />
+    <FormTextFieldElement
+      required
+      fullWidth
+      margin={"dense"}
+      label={"Last Name"}
+      name={"default-last-name-field"}
+    />
+    <br />
+    <FormTextFieldElement
+      required
+      fullWidth
+      type={"email"}
+      margin={"dense"}
+      label={"Email"}
+      name={"default-email-field"}
+    />
+  </>
+);
