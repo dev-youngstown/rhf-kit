9c8cb871fd72eecc95cb3fadb5c98138a7265cb5
nicolasjott <nicobox2002@gmail.com>
2024-04-18 15:39:01 -0400

    Add some typescript documentation

--- packages/mui/src/FormTextFieldElement/FormTextFieldElement.tsx
+++ packages/mui/src/FormTextFieldElement/FormTextFieldElement.tsx
@@ -11,7 +11,24 @@ type FormTextFieldElementComponent = <
     RefAttributes<HTMLDivElement>
 ) => JSX.Element;
 
-const FormTextFieldElement = forwardRef(function TextFieldElement<
+/**
+ * The `FormTextFieldElement` component is a wrapper around the MUI `TextField`.
+ * This component that serves as a layer of abstraction form any `react-hook-form` form control functions.
+ * and validation in MUI-based forms. This component simplifies the use of text fields within a form context,
+ * handling errors and custom validation rules transparently.
+ *
+ *
+ * ## Advanced Configuration
+ * This component accepts all the props that the MUI `TextField` component accepts.
+ *
+ * The component is implemented using:
+ * - [MUI TextField](https://mui.com/material-ui/react-text-field/)
+ * - [React Hook Form Controller](https://react-hook-form.com/docs/usecontroller)
+ *
+ * TODO: Add example usage link to Storybook docs
+ */
+
+const FormTextFieldElement = forwardRef(function FormTextFieldElement<
   TFieldValues extends FieldValues = FieldValues,
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
 >(
