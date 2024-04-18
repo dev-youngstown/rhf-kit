5c94f81af4bbf41d9eaccfde0626d05abdcc736c
nicolasjott <nicobox2002@gmail.com>
2024-04-18 15:15:44 -0400

    Add Localization provider for mui date picker

--- packages/mui/src/StoryBook/FormDecorator.tsx
+++ packages/mui/src/StoryBook/FormDecorator.tsx
@@ -1,19 +1,23 @@
 import FormContainer from "@/FormContainer";
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
 import { action } from "@storybook/addon-actions";
 import { SubmitButton } from "./SubmitButton";
 
 export function FormContainerDecorator(Story) {
   return (
-    <FormContainer
-      defaultValues={{
-        preset: "2023-02-01",
-        datetime: null,
-        wrong_date: "2023-13-199",
-      }}
-      onSuccess={action("submit")}
-    >
-      <Story />
-      <SubmitButton />
-    </FormContainer>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <FormContainer
+        defaultValues={{
+          preset: "2023-02-01",
+          datetime: null,
+          wrong_date: "2023-13-199",
+        }}
+        onSuccess={action("submit")}
+      >
+        <Story />
+        <SubmitButton />
+      </FormContainer>
+    </LocalizationProvider>
   );
 }
