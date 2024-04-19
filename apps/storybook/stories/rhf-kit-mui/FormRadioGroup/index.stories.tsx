764da8dca761eaced02ef6da11c70c8a5f53158e
nicolasjott <nicobox2002@gmail.com>
2024-04-19 16:18:06 -0400

    create storybook docs for form radio group

---
+++ apps/storybook/stories/rhf-kit-mui/FormRadioGroup/index.stories.tsx
@@ -0,0 +1,41 @@
+import { FormContainerDecorator } from "@/FormDecorator";
+import { FormRadioGroup, FormRadioOption } from "@rhf-kit/mui";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Radio Group",
+  component: FormRadioGroup,
+  decorators: [FormContainerDecorator],
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof FormRadioGroup>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const options: FormRadioOption[] = [
+  { label: "Option 1", value: 1 },
+  { label: "Option 2", value: 2 },
+  { label: "Option 3", value: 3 },
+  { label: "Option 4", value: 4 },
+];
+
+export const Basic: Story = {
+  args: {
+    label: "Radio Group",
+    name: "radioGroupBase",
+    options,
+  },
+};
+
+export const Required: Story = {
+  args: {
+    label: "Radio Group",
+    name: "radioGroupRequired",
+    options,
+    required: true,
+  },
+};
