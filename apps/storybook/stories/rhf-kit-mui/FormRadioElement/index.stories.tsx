62282c25dc7d48d7fe4006dcb8c555b6a7f7dd81
nicolasjott <nicobox2002@gmail.com>
2024-04-20 14:34:47 -0400

    stories for form radio element

---
+++ apps/storybook/stories/rhf-kit-mui/FormRadioElement/index.stories.tsx
@@ -0,0 +1,54 @@
+import { FormContainerDecorator } from "@/FormDecorator";
+import { FormRadioElement } from "@rhf-kit/mui";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  title: "Form Radio",
+  component: FormRadioElement,
+  decorators: [FormContainerDecorator],
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof FormRadioElement>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Basic: Story = {
+  args: {
+    name: "radioBase",
+    value: true,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    name: "radioDisabled",
+    value: false,
+    disabled: true,
+  },
+};
+
+export const WithLabel: Story = {
+  args: {
+    name: "radioLabel",
+    label: "Radio Label",
+    value: true,
+  },
+};
+
+export const StringValue: Story = {
+  args: {
+    name: "radioStringValue",
+    value: "string",
+  },
+};
+
+export const NumberValue: Story = {
+  args: {
+    name: "radioNumberValue",
+    value: 1,
+  },
+};
