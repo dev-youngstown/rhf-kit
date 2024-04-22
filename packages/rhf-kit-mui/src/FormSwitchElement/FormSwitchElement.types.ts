20242cf8263aa917be8f310d294608ee640e15e0
nicolasjott <nicobox2002@gmail.com>
2024-04-22 17:57:53 -0400

    Update FormSwitchElement types and props

--- packages/rhf-kit-mui/src/FormSwitchElement/FormSwitchElement.types.ts
+++ packages/rhf-kit-mui/src/FormSwitchElement/FormSwitchElement.types.ts
@@ -1,4 +1,9 @@
-import { SwitchProps } from "@mui/material";
+import {
+  FormControlLabelProps,
+  FormControlProps,
+  FormHelperTextTypeMap,
+  SwitchProps,
+} from "@mui/material";
 import { Control, FieldPath, FieldValues } from "react-hook-form";
 
 export type FormSwitchElementProps<
@@ -29,4 +34,22 @@ export type FormSwitchElementProps<
    * Text that is displayed below the switch.
    */
   helperText?: string;
+  /**
+   * props passed to the MUI `FormControl` component.
+   */
+  formControlProps?: Omit<
+    FormControlProps,
+    "error" | "required" | "disabled" | "ref"
+  >;
+  /**
+   * props passed to the MUI `FormControlLabel` component.
+   */
+  formControlLabelProps?: Omit<
+    FormControlLabelProps,
+    "control" | "required" | "label" | "inputRef"
+  >;
+  /**
+   * props passed to the MUI `FormHelperText` component.
+   */
+  formHelperTextProps?: Omit<FormHelperTextTypeMap, "error">;
 };
